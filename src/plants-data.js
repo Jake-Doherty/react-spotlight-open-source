e5bb7d286fb84d6ecb54c41bd06ccfd6508a209e
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-31 12:13:00 -0800

    adds plants data

---
+++ src/plants-data.js
@@ -0,0 +1,64 @@
+const plants = [
+  {
+    id: 1,
+    common_name: 'Mustang Monardella',
+    plant_family: 'Lamiaceae',
+    plant_scientific_name: 'Monardella lanceolata A. Gray var. microcephala A. Gray',
+  },
+  {
+    id: 2,
+    common_name: 'Silky Lupine',
+    plant_family: 'Fabaceae',
+    plant_scientific_name: 'Lupinus sericeus Pursh ssp. sericeus',
+  },
+  {
+    id: 3,
+    common_name: 'Mt. Hamilton Mock Stonecrop',
+    plant_family: 'Crassulaceae',
+    plant_scientific_name: 'Sedella pentandra H. Sharsm.',
+  },
+  {
+    id: 4,
+    common_name: 'Puerto Rico Stopper',
+    plant_family: 'Myrtaceae',
+    plant_scientific_name: 'Eugenia bellonis Krug & Urb.',
+  },
+  {
+    id: 5,
+    common_name: 'Catch Me If You Can',
+    plant_family: 'Euphorbiaceae',
+    plant_scientific_name: 'Acalypha amentacea Roxb.',
+  },
+  {
+    id: 6,
+    common_name: 'Marsh Sandwort',
+    plant_family: 'Caryophyllaceae',
+    plant_scientific_name: 'Arenaria paludicola B.L. Rob.',
+  },
+  {
+    id: 7,
+    common_name: 'Pohlia Moss',
+    plant_family: 'Bryaceae',
+    plant_scientific_name: 'Pohlia melanodon (Brid.) Shaw',
+  },
+  {
+    id: 8,
+    common_name: 'Syngonanthus',
+    plant_family: 'Eriocaulaceae',
+    plant_scientific_name: 'Syngonanthus Ruhl.',
+  },
+  {
+    id: 9,
+    common_name: 'Pinkflower Hedgehog Cactus',
+    plant_family: 'Cactaceae',
+    plant_scientific_name: 'Echinocereus fendleri (Engelm.) Sencke ex J.N. Haage ssp. fendleri',
+  },
+  {
+    id: 10,
+    common_name: 'Moco De Pavo',
+    plant_family: 'Poaceae',
+    plant_scientific_name: 'Eriochrysis cayennensis P. Beauv.',
+  },
+];
+
+export default plants;
